d6e128ae6fa35356e727a09bcb692a458099fd25
pathikritsyam <pathikritsyam@gmail.com>
2023-04-10 11:53:22 +0530

    commit

--- s3q4.m
+++ s3q4.m
@@ -7,25 +7,25 @@ fmt = [repmat('%8.4f ', 1, perLine), '\n'];
 i=1; 
 figure;
 for n=[21,41] 
-    fc=2000; 
+    flo=2500; 
+    fhi=3000; 
     fs=8000; 
     nr=fs/2; 
-    Wn=fc/nr; 
-    f=[0 Wn Wn 1]; 
-    m=[1 1 0 0]; 
+    f=[0 flo/nr-0.1 flo/nr fhi/nr fhi/nr+0.1 1]; 
+    m=[0 0 1 1 0 0]; 
     b=fir2(n-1,f,m);
     fprintf('1 x ');
     fprintf(string(n));
     fprintf('\n');
     fprintf(fmt, b);
     if mod( length(b), perLine) ~= 0; fprintf('\n'); end
-    [h,w]=freqz(b); 
+    [h,w]=freqz(b);  
     subplot(2,2,i); 
     i=i+1;
-    plot(w/pi,20*log10(abs(h))); 
+    plot(w/pi,abs(h)); 
     title(["Magnitude response for filter of length ",n]); 
     subplot(2,2,i); 
-    i=i+1;
     plot(w/pi, angle(h)); 
     title(["Phase response for filter of length ",n]); 
+    i=i+1; 
 end 
